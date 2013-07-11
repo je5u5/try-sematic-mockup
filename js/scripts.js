4d485365a7bd7af60c43bf7084cd3a3d2c8a724f
HeX <hexcaballero@gmail.com>
2013-07-11 18:48:06 -0500

    Nuestras Oficinas

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
 $("document").ready( function() {
 	$("#scrollinterno").niceScroll({cursorcolor:"#C2ECE2", cursorwidth:"7px", autohidemode:"false", background:"#C0D3CF", railoffset: {top:0, left:-3}});
+	$("#zona").niceScroll({cursorcolor:"#C2ECE2", autohidemode:"false", background:"#C0D3CF", railoffset: {top:0, left:-3}});
+	$("#tipo").niceScroll({cursorcolor:"#C2ECE2", autohidemode:"false", background:"#C0D3CF", railoffset: {top:0, left:-3}});
+	assignEvents();
 });
+
+function assignEvents() {
+	//Filtro - Compra/Renta
+	$("#filtro a").click(function(){
+		$("#filtro a").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	//Filtros - drop-down
+	$("#filtro>div>div span").click(function(){
+		if ($(this).parent().children("div").hasClass("show")) {
+			$(this).parent().children("div").removeClass("show");
+		} else {
+			$(this).parent().children("div").addClass("show");
+			$("#zona").niceScroll().resize();
+			$("#tipo").niceScroll().resize();
+		}
+	});
+
+	//Filtros - drop-down
+	$("#filtro li").click(function(index){
+		console.log( index + ": " + $(this).text() );
+		$("#filtro>div>div").removeClass("show");
+/*		$(this).parent().children("span").text = $("this").text()
+		console.log($(this).parent().text());
+		$(this).parent().addClass("seleccionado");*/
+	});
+}
