8a3afa47811018ec1dbde87b4cace8b3b345713d
HeX <hexcaballero@gmail.com>
2013-07-11 11:07:41 -0500

    Compra-Renta1
    - Limpie archivos sin usar (/index, /header)

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+$("document").ready( function() {
+	$("#scrollinterno").niceScroll({cursorcolor:"#C2ECE2", cursorwidth:"7px", autohidemode:"false", background:"#C0D3CF", railoffset: {top:0, left:-3}});
+});
