2ec7c197dc790faf5db34aa65a23a4f56276beef
Jesus Meza <jsmsmail@gmail.com>
2013-07-11 23:35:30 -0500

    seccion desarrollo editada

--- js/all.js
+++ js/all.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
 	$("#carousel img").click(function() {
 	    $("#picture img").attr("src", $(this).attr("src"));
 	});
+	$("#placeslist").jCarouselLite({
+		btnNext: "#go",
+		btnPrev: "#back",
+		visible: 1,
+		afterEnd: function (a){
+			title = a.find("span").attr("title")
+			src = a.find("span").attr("src")
+
+			$("#logos img").attr("src",src)
+			$("#logos span").html(title)
+		}
+	});
 });
